fecca573d33a44c7254b7ee938b02776c60448f2
Aleksandar Tsvetkov <aleksandar.tsvetkov@sap.com>
2024-07-20 16:52:18 +0300

    Allow sorting of developers

--- src/app/developers/developers.component.ts
+++ src/app/developers/developers.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { Developer } from '../model/Developer';
+import { PaginatedResponse } from '../model/PaginatedResponse';
 import { DevelopersService } from '../services/developers.service';
 
 @Component({
+  selector: 'fr-developers',
   templateUrl: './developers.component.html',
   styleUrls: ['./developers.component.css']
 })
@@ -18,31 +21,59 @@ export class DevelopersComponent implements OnInit {
 
   developers: Developer[] = [];
   developersLoading = true;
-  page = 0;
+  currentPage = 0;
   totalPages = 0;
+  sortField = 'score';
+  sortDirection = 'asc';
 
   constructor(private developersService: DevelopersService) { }
 
   ngOnInit(): void {
-    this.fetchDevelopers(this.page, this.pageSize);
+    this.fetchPage(this.currentPage, this.pageSize).subscribe(page => this.vizualizePage(page));
   }
 
-  private fetchDevelopers(page: number, size: number): void {
+  private fetchPage(page: number, pageSize: number): Observable<PaginatedResponse<Developer>> {
     this.developersLoading = true;
 
-    this.developersService.getDevelopers(page, size).subscribe(developersPage => {
-      console.log(`Fetched developers: ${JSON.stringify(developersPage)}`);
-      this.developers = [...this.developers, ...developersPage.content];
-      this.totalPages = developersPage.totalPages;
-      this.developersLoading = false;
-      this.page = page;
-    });
+    return this.developersService.getDevelopers(page, pageSize, this.sortField, this.sortDirection).pipe(
+      // delay(2000), // Uncomment to test the loading indicator
+      tap(page => console.log(`Fetched developers page: ${JSON.stringify(page)}`)),
+      tap(page => this.developersLoading = false)
+    );
+  }
+
+  private vizualizePage(page: PaginatedResponse<Developer>): void {
+    this.developers = [...this.developers, ...page.content];
+    this.totalPages = page.totalPages;
+    this.currentPage = page.number;
   }
 
   loadMore(): void {
-    if (this.page < this.totalPages - 1) {
-      this.fetchDevelopers(this.page + 1, this.pageSize);
+    if (this.currentPage < this.totalPages - 1) {
+      this.fetchPage(this.currentPage + 1, this.pageSize).subscribe(page => this.vizualizePage(page));
     }
+  }
+
+  onSortChange(field: string): void {
+    if (this.developersLoading) {
+      return;
+    }
+    if (this.sortField === field) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortField = field;
+      this.sortDirection = 'asc';  // Reset to ascending if a new field is clicked
+    }
+    this.fetchPage(0, (this.currentPage + 1) * this.pageSize).subscribe(page => {
+      this.developers = page.content;
+    });
+  }
+
+  getSortArrowClass(field: string): string {
+    if (this.sortField === field) {
+      return this.sortDirection === 'asc' ? 'arrow-up' : 'arrow-down';
+    }
+    return '';
   }
 
 }
