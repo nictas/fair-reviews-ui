c7213fc0d119f2b92db499b6b054af7501dfc91d
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-07-21 18:31:57 +0300

    Implement multipliers page

---
+++ src/app/services/multipliers.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
+import { Multiplier } from '../model/Multiplier';
+import { PaginatedResponse } from '../model/PaginatedResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MultipliersService {
+
+  private gatewayUrl = environment.gatewayUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getMultipliers(page: number, size: number, sort: string, direction: string): Observable<PaginatedResponse<Multiplier>> {
+    let url = `${this.gatewayUrl}/rest/multipliers?page=${page}&size=${size}&sort=${sort},${direction}&sort=id,asc`;
+    return this.httpClient.get<PaginatedResponse<Multiplier>>(url);
+  }
+
+  deleteMultiplier(id: string): Observable<void> {
+    return this.httpClient.delete<void>(`${this.gatewayUrl}/rest/multipliers/${id}`);
+  }
+}
