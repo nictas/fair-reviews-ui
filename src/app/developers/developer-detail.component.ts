60fb5eb39c6d7b3f2425c3953431bb822e3f5b9c
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-07-22 20:30:08 +0300

    Implement pagination of developer history

--- src/app/developers/developer-detail.component.ts
+++ src/app/developers/developer-detail.component.ts
@@ -1,10 +1,11 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { delay, tap } from 'rxjs';
+import { delay, Observable, tap } from 'rxjs';
 import { Developer } from '../model/Developer';
 import { PullRequestReview } from '../model/PullRequestReview';
 import { DevelopersService } from '../services/developers.service';
+import { PaginatedResponse } from '../model/PaginatedResponse';
 
 @Component({
   templateUrl: './developer-detail.component.html',
@@ -12,8 +13,20 @@ import { DevelopersService } from '../services/developers.service';
 })
 export class DeveloperDetailComponent implements OnInit {
 
+  login = '';
   developer: Developer | null = null;
-  reviews: PullRequestReview[] = [];
+
+  private _reviews: PullRequestReview[] = [];
+
+  get reviews(): PullRequestReview[] {
+    return this._reviews;
+  }
+
+  set reviews(reviews: PullRequestReview[]) {
+    this._reviews = reviews;
+    this.updateShown();
+  }
+
   shownReviews: PullRequestReview[] = [];
   dataLoading = true;
   isCollapsed = false;
@@ -28,6 +41,13 @@ export class DeveloperDetailComponent implements OnInit {
     this.updateShown();
   }
 
+  get pageSize() {
+    return 5;
+  }
+
+  currentPage = 0;
+  totalPages = 0;
+
   constructor(
     private developersService: DevelopersService,
     private route: ActivatedRoute,
@@ -35,26 +55,42 @@ export class DeveloperDetailComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const login = this.route.snapshot.paramMap.get('login');
-    if (login) {
+    const loginParameter = this.route.snapshot.paramMap.get('login');
+    if (loginParameter != null) {
+      this.login = loginParameter;
+    }
+    if (this.login) {
       this.dataLoading = true;
-      this.developersService.getDeveloper(login).pipe(
+      this.developersService.getDeveloper(this.login).pipe(
         tap(data => console.log(`Fetched data: ${JSON.stringify(data)}`))
       ).subscribe(developer => {
         this.developer = developer;
-        this.developersService.getDeveloperHistory(login).pipe(
-          delay(2000), // Uncomment to test the loading indicator
-          tap(data => console.log(`Fetched data: ${JSON.stringify(data)}`)),
-          tap(data => {
-            this.dataLoading = false;
-          })
-        ).subscribe(reviews => {
-          this.reviews = reviews.content;
-          this.updateShown();
-        });
+        this.fetchPage(this.login, this.currentPage, this.pageSize).subscribe(page => this.vizualizePage(page));
       })
-
     }
+  }
+
+  loadMore(): void {
+    if (this.currentPage < this.totalPages - 1) {
+      this.fetchPage(this.login, this.currentPage + 1, this.pageSize).subscribe(page => this.vizualizePage(page));
+    }
+  }
+
+  private vizualizePage(page: PaginatedResponse<PullRequestReview>): void {
+    this.reviews = [...this.reviews, ...page.content];
+    this.totalPages = page.totalPages;
+    this.currentPage = page.number;
+  }
+
+  fetchPage(login: string, page: number, pageSize: number): Observable<PaginatedResponse<PullRequestReview>> {
+    this.dataLoading = true;
+    return this.developersService.getDeveloperHistory(login, page, pageSize).pipe(
+      delay(2000), // Uncomment to test the loading indicator
+      tap(data => console.log(`Fetched data: ${JSON.stringify(data)}`)),
+      tap(data => {
+        this.dataLoading = false;
+      })
+    );
   }
 
   updateShown(): void {
