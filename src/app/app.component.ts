26d0bc2e044bd5c70b16a5588390f4ff54733bee
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-07-25 17:07:35 +0300

    Show error messages when authenticating the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,39 +3,43 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserInfo } from './model/UserInfo';
 import { UserInfoService } from './services/user-info.service';
+import { catchError, of } from 'rxjs';
+import { MessageBaseComponent } from './shared/message-base.component';
 
 @Component({
   selector: 'fr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent extends MessageBaseComponent implements OnInit {
   isUserAuthenticated = false;
-  userLogin: string | undefined;
+  user: string | undefined;
   userRole: 'Administrator' | 'User' | undefined;
 
-  constructor(private userInfoService: UserInfoService, private router: Router) { }
+  constructor(private userInfoService: UserInfoService, private router: Router) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.userInfoService.getUserInfo().subscribe(
-      (userInfo: UserInfo) => {
-        // If successful, store user info
-        this.isUserAuthenticated = true;
-        this.userLogin = userInfo.login;
-        this.userRole = userInfo.roles.includes('ROLE_ADMIN') ? 'Administrator' : 'User';
-      },
-      (error: HttpErrorResponse) => {
-        console.log('An error occurred:', error);
+    this.userInfoService.getUserInfo().pipe(
+      catchError(error => {
         if (error.status === 401) {
-          // If 401, redirect to /login-github
           console.log('Redirecting to /login-github');
           this.router.navigate(['/login-github']);
         } else {
-          // Handle other errors or log them if necessary
-          console.error('An error occurred:', error);
+          // Not using this.showFailureMessage() to keep the message on the screen until the page is refreshed.
+          this.message = `An error occurred while communicating with the server. Refreshing this page might help. Error details: ${error.message}`;
+          this.messageType = 'failure';
         }
+        return of(null);
+      })
+    ).subscribe(userInfo => {
+      if (userInfo) {
+        this.isUserAuthenticated = true;
+        this.user = userInfo.login;
+        this.userRole = userInfo.roles.includes('ROLE_ADMIN') ? 'Administrator' : 'User';
       }
-    );
+    });
   }
 
   logout() {
