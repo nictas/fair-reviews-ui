6d22681cec64cde59d4852918f51c6b424feb744
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-08-19 12:33:04 +0300

    Simplify login redirection

--- src/environment/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    gatewayUrl: 'https://fair-reviews.cfapps.eu10.hana.ondemand.com'
-};
--- src/environment/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    gatewayUrl: 'http://localhost:8090'
-};
